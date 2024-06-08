a4c75acb1744c7e9656aaf1140e2428a77fbc2ce
Stoyan Nikolchev <nikolchev98@protonmail.com>
2024-06-08 14:51:49 +0300

    Add like button to the Quote of the Day on the home page.

--- src/main/java/com/softuni/quotependium/web/HomeController.java
+++ src/main/java/com/softuni/quotependium/web/HomeController.java
@@ -2,6 +2,7 @@ package com.softuni.quotependium.web;
 
 import com.softuni.quotependium.domain.views.QuoteView;
 import com.softuni.quotependium.services.QuoteOfTheDayService;
+import com.softuni.quotependium.services.UserService;
 import com.softuni.quotependium.services.schedulers.QuoteOfTheDayScheduler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -11,16 +12,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
     private final QuoteOfTheDayService quoteOfTheDayService;
+    private final UserService userService;
 
     @Autowired
-    public HomeController(QuoteOfTheDayScheduler quoteOfTheDayScheduler, QuoteOfTheDayService quoteOfTheDayService) {
+    public HomeController(QuoteOfTheDayScheduler quoteOfTheDayScheduler, QuoteOfTheDayService quoteOfTheDayService, UserService userService) {
         this.quoteOfTheDayService = quoteOfTheDayService;
+        this.userService = userService;
     }
 
     @GetMapping("/")
     public String getHome(Model model) {
         QuoteView quoteOfTheDay = this.quoteOfTheDayService.getQuoteOfTheDayView();
         model.addAttribute("quoteOfTheDay", quoteOfTheDay);
+
+        Long currentUserId = this.userService.getCurrentUserId();
+        model.addAttribute("currentUserId", currentUserId);
         return "index";
     }
 }
