cd44d132f7873656a47b7e232e9b783cd5edeb10
Stoyan Nikolchev <nikolchev98@protonmail.com>
2024-06-13 18:40:13 +0300

    Additional IT tests for the Browse Controller.

--- src/test/java/com/softuni/quotependium/web/BrowseControllerTestIT.java
+++ src/test/java/com/softuni/quotependium/web/BrowseControllerTestIT.java
@@ -1,8 +1,11 @@
 package com.softuni.quotependium.web;
 
 import com.softuni.quotependium.domain.views.AuthorView;
+import com.softuni.quotependium.domain.views.BookDetailsView;
 import com.softuni.quotependium.services.AuthorService;
 import com.softuni.quotependium.services.BookService;
+import com.softuni.quotependium.services.QuoteService;
+import com.softuni.quotependium.services.UserService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -13,6 +16,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -31,12 +35,19 @@ public class BrowseControllerTestIT {
     @MockBean
     private BookService bookService;
 
+    @MockBean
+    private QuoteService quoteService;
+
+    @MockBean
+    private UserService userService;
+
     @Test
     public void getAllAuthors_shouldReturnBrowseAuthors() throws Exception {
         when(authorService.getAllAuthors(any(Pageable.class))).thenReturn(mock(Page.class));
 
         mockMvc.perform(get("/browse/authors"))
                 .andExpect(model().attributeExists("authors"))
+                .andExpect(status().isOk())
                 .andExpect(view().name("browse-authors"));
     }
 
@@ -48,6 +59,7 @@ public class BrowseControllerTestIT {
         mockMvc.perform(get("/browse/authors/1"))
                 .andExpect(model().attributeExists("author"))
                 .andExpect(model().attributeExists("books"))
+                .andExpect(status().isOk())
                 .andExpect(view().name("author-details"));
     }
 
@@ -57,5 +69,47 @@ public class BrowseControllerTestIT {
 
         mockMvc.perform(get("/browse/authors/1"))
                 .andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void getAllQuotes_shouldReturnBrowseQuotes() throws Exception {
+        when(quoteService.getAllQuotes(any(Pageable.class))).thenReturn(mock(Page.class));
+        when(userService.getCurrentUserId()).thenReturn(1L);
+
+        mockMvc.perform(get("/browse/quotes"))
+                .andExpect(model().attributeExists("currentUserId", "quotes"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("browse-quotes"));
+
+    }
+
+    @Test
+    public void getAllBooks_shouldReturn_browseBooks() throws Exception {
+        when(bookService.getAllTitles(any(Pageable.class))).thenReturn(mock(Page.class));
+        when(userService.getCurrentUserId()).thenReturn(1L);
+
+        mockMvc.perform(get("/browse/books"))
+                .andExpect(model().attributeExists("currentUserId", "bookTitles"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("browse-books"));
+    }
+
+    @Test
+    public void getBookDetails_whenBookDoesNotExist_shouldReturnNotFound() throws Exception {
+        when(bookService.findBookById(1L)).thenReturn(null);
+
+        mockMvc.perform(get("/browse/books/1"))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void getBookDetails_whenBookExists_shouldReturnBookDetails() throws Exception {
+        when(bookService.findBookById(1L)).thenReturn(mock(BookDetailsView.class));
+        when(quoteService.getAllQuotesByBookId(eq(1L), any(Pageable.class))).thenReturn(mock(Page.class));
+
+        mockMvc.perform(get("/browse/books/1"))
+                .andExpect(model().attributeExists("book", "quotes"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("book-details"));
     }
 }
