64f33b330e8421e136e27c2bd26355ccdd221c57
StoyanNikolchev <nikolchev98@protonmail.com>
2024-08-17 14:30:38 +0300

    Fix add-quote page not passing bookId request param upon redirecting when bindingResult has errors.

--- src/main/java/com/softuni/quotependium/web/QuoteController.java
+++ src/main/java/com/softuni/quotependium/web/QuoteController.java
@@ -8,6 +8,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -28,15 +29,20 @@ public class QuoteController {
 
     @GetMapping("/quotes/add")
     public String getAddQuote(@ModelAttribute("quoteImportDto") QuoteImportDto quoteImportDto,
-                              @RequestParam("bookId") Long bookId) {
+                              @RequestParam("bookId") Long bookId,
+                              Model model) {
+        model.addAttribute("bookId", bookId);
         return "add-quote";
     }
 
     @PostMapping("/quotes/add")
-    public String postQuote(@Valid QuoteImportDto quoteImportDto,
-                            BindingResult bindingResult) {
+    public String postQuote(@Valid @ModelAttribute("quoteImportDto") QuoteImportDto quoteImportDto,
+                            BindingResult bindingResult,
+                            @RequestParam("bookId") Long bookId,
+                            Model model) {
 
         if (bindingResult.hasErrors()) {
+            model.addAttribute("bookId", bookId);
             return "add-quote";
         }
 
